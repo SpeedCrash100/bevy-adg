c3281ae21ee0b402a69fdebe28c90b186119ebc1
SpeedCrash100 <SpeedCrash100@yandex.ru>
2023-01-26 06:10:43 +0300

    Add component injector builder

--- game/src/entity.rs
+++ game/src/entity.rs
@@ -1,6 +1,6 @@
 use bevy::{
     ecs::system::{Commands, EntityCommands},
-    prelude::{ChildBuilder, Entity},
+    prelude::{Bundle, ChildBuilder, Entity},
 };
 
 /// Builds complex entity using provided commands
@@ -27,5 +27,26 @@ impl<'w, 's, 'a> EntityBuildDirector for ChildBuilder<'w, 's, 'a> {
     fn build_entity<B: EntityBuilder>(&mut self, builder: &B) -> Entity {
         let mut commands = self.spawn(());
         builder.build(&mut commands).id()
+    }
+}
+
+/// Allows to inject component to entity builded by another builder
+pub struct ComponentInjectorBuilder<B: EntityBuilder, I: Bundle + Clone> {
+    builder: B,
+    injectee: I,
+}
+
+impl<B: EntityBuilder, I: Bundle + Clone> ComponentInjectorBuilder<B, I> {
+    pub fn new(builder: B, injectee: I) -> Self {
+        Self { builder, injectee }
+    }
+}
+
+impl<B: EntityBuilder, I: Bundle + Clone> EntityBuilder for ComponentInjectorBuilder<B, I> {
+    fn build<'w, 's, 'a, 'c>(
+        &self,
+        commands: &'c mut EntityCommands<'w, 's, 'a>,
+    ) -> &'c mut EntityCommands<'w, 's, 'a> {
+        self.builder.build(commands).insert(self.injectee.clone())
     }
 }
